9f2f0ee3030724174983975ac9942459ab8f23ee
huangfl <hflhhb@126.com>
2019-07-19 09:28:15 +0800

    upgrade project dotnetcore version

--- LLY.LifeTask.Office/Startup.cs
+++ LLY.LifeTask.Office/Startup.cs
@@ -16,6 +16,9 @@ using LLY.LifeTask.Dapper;
 using LLY.LifeTask.Common;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 
 namespace LLY.LifeTask.Office
 {
@@ -65,7 +68,8 @@ namespace LLY.LifeTask.Office
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+            var logger = loggerFactory.CreateLogger<Startup>();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -78,12 +82,77 @@ namespace LLY.LifeTask.Office
 
             app.UseStaticFiles();
 
+            app.Use(async (context, next) =>{
+
+                //await Task.Delay(0);
+
+                logger.LogInformation("");
+                var s = context.Request.Cookies["sessionId"];
+
+                Extensions.AddUserCookies(context, "SDKSessionId", "xxafeinjqwetrqwe", "SDKSession" );
+                context.Response.Cookies.Append("sessionSDK", "ssss");
+                await next.Invoke();
+            });
+
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void AddUserCookies(HttpContext httpContext, string key, string value, string cookiename)
+        {
+            //原始代码
+            //var cookie = httpContext.Request.Cookies[cookiename];
+            //if (cookie != null)
+            //    httpContext.Request.Cookies.Remove(cookiename);
+            //cookie = new HttpCookie(cookiename) {Expires = DateTime.Now.AddHours(4), [key] = value};
+            //httpContext.Response.AppendCookie(cookie);
+
+            if (httpContext.Request.Cookies.ContainsKey(cookiename))
+            {
+                httpContext.Response.Cookies.Delete(cookiename);
+            }
+            httpContext.Response.Headers.AppendRawValueCookie(cookiename, new Dictionary<string, string>{{key, value}}.ToLegacyCookieString(), new CookieOptions
+            {
+                Expires = DateTime.Now.AddHours(4)
+            });
+        }
+
+
+        public static void AppendRawValueCookie(this IHeaderDictionary headers, string key, string value, CookieOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+            SetCookieHeaderValue cookieHeaderValue = new SetCookieHeaderValue(Uri.EscapeDataString(key), value)
+            {
+                Domain =  options.Domain,
+                Path =  options.Path,
+                Expires = options.Expires
+            };
+            int num1 = options.Secure ? 1 : 0;
+            cookieHeaderValue.Secure = num1 != 0;
+            int sameSite = (int)options.SameSite;
+            cookieHeaderValue.SameSite = (Microsoft.Net.Http.Headers.SameSiteMode)sameSite;
+            int num2 = options.HttpOnly ? 1 : 0;
+            cookieHeaderValue.HttpOnly = num2 != 0;
+            headers["Set-Cookie"] = StringValues.Concat(headers["Set-Cookie"], (StringValues)cookieHeaderValue.ToString());
+        }
+
+        public static IDictionary<string, string> FromLegacyCookieString(this string legacyCookie)
+        {
+            return legacyCookie.Split('&').Select(s => s.Split('=')).ToDictionary(kvp => kvp[0], kvp => kvp[1]);
+        }
+
+        public static string ToLegacyCookieString(this IDictionary<string, string> dict)
+        {
+            return string.Join("&", dict.Select(kvp => string.Join("=", kvp.Key, kvp.Value)));
         }
     }
 }
